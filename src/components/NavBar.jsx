41a94d4ad939611257101ac1526ded5f925ce390
Shivam-136 <shivam243028@gmail.com>
2026-04-26 18:43:05 +0530

    E-Comm

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
         {loggedInUser ? (
           <button 
             onClick={handleLogout}
-            className='bg-red-500 px-4 py-2 rounded-lg hover:bg-red-600 transition'
+            className='bg-red-400 px-4 py-2 rounded-lg hover:bg-red-600 transition'
           >
             Logout
           </button>
